7130f81a1d5a3ce6534104be667c9ddbb46b580b
Trung Hiếu <nguyenminhtrunghieu694@gmail.com>
2024-10-14 16:02:21 +0700

    Init Project

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,7 +3,7 @@ import "./HomePage.scss"
 function HomePage() {
   return (
     <div className="homepage">
-      <h1>Welcome to Home Page</h1>
+      <h1>Welcome to Home Page New</h1>
     </div>
   );
 }
